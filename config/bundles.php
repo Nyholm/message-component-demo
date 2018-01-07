c76285b7825a346a1e15a4e54ca5d3e1c8e17af7
Nyholm <tobias.nyholm@gmail.com>
2018-01-07 19:51:52 +0100

    init

---
+++ config/bundles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Sam\Symfony\Bridge\EnqueueMessage\Bundle\EnqueueBridgeBundle::class => ['all' => true],
+];
