c5b6c9b627e25ea0c5c1a2bf62b85f5661d91981
Samuel ROZE <samuel.roze@gmail.com>
2018-03-27 19:10:39 +0200

    Uses HandlerInterface for command handlers

--- src/Message/CommandHandler/CreateNumberHandler.php
+++ src/Message/CommandHandler/CreateNumberHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Message\CommandHandler;
 
 use App\Message\Command\CreateNumber;
+use Symfony\Component\Messenger\Handler\HandlerInterface;
 
 /**
  * This command handler handles message asynchronously. See config in config/packages/framework.yaml
@@ -12,10 +13,19 @@ use App\Message\Command\CreateNumber;
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-class CreateNumberHandler
+class CreateNumberHandler implements HandlerInterface
 {
-    public function __invoke(CreateNumber $command)
+    /**
+     * {@inheritdoc}
+     */
+    public static function getHandledMessages(): array
     {
+        return [CreateNumber::class];
+    }
+
+    public function __invoke($command)
+    {
+        /** @var CreateNumber $command */
         $number = rand($command->getMin(), $command->getMax());
 
         // TODO Store in database... or whatever
